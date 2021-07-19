5d555180cee4687d6b8eae0b1d1c2b0db97e1ebe
IMRAN BAALI <imran.baali19@gmail.com>
2021-07-19 18:57:18 +0100

    finish figma design

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+
+$(function () {
+	'use strict';
+	$("#list-files").on("click", function () {
+		$(".get-files-section").removeClass("d-none");
+	});
+	
+	$("#close-get-files, .get-files-section").on("click", function () {
+		$(".get-files-section").addClass("d-none");
+	});
+
+	$(".get-files-section .files-preview").on("click", function (event) {
+		event.stopPropagation();
+	})
+
+	$("#settings").on("click", function () {
+		$(".settings-section").removeClass("d-none");
+	});
+
+	$("#close-get-settings, .settings-section").on("click", function () {
+		$(".settings-section").addClass("d-none");
+	});
+
+	$(".settings-section .settings-preview").on("click", function (event) {
+		event.stopPropagation();
+	})
+
+	$(".html, .css, .js, .assets").on("click", function() {
+		$(this).addClass('active').siblings().removeClass('active');;
+	})
+})
