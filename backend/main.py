1fd0ddbbd5b79d51b4ec02af3484a5c066fbf94b
IMRAN BAALI <imran.baali19@gmail.com>
2021-07-24 19:20:15 +0100

    add saving js css images files localy

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,23 @@
-# from bs4 import BeautifulSoup
-
-# import requests
-# r = requests.get("https://kirwako.com")
-# data = r.text
-# soup = BeautifulSoup(data)
-
-# for link in soup.find_all('a'):
-# print(link.get('href'))
-
-
 # Import Required Library
-from requests import get
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
+from	os								import	link
+from	requests						import	get
+from	bs4								import	BeautifulSoup
+from	urllib.parse					import	urlparse
+from	create_public_assets_directory	import	create_public_assets_directory, save_web_site_file
+from	pathlib							import	Path
+from	os								import	path
+
+create_public_assets_directory()
 
 # Web URL
-web_url = "https://www.kirwako.com/"
+# web_url = "https://www.kirwako.com/"
+web_url = "https://bookylia.com/"
 # web_url = "https://www.geeksforgeeks.org/how-to-extract-script-and-css-files-from-web-pages-in-python/"
 
 # get HTML content
-html = get(web_url).content
+url = get(web_url)
+html = url.content
+# text = url.text
 
 web_url_domain = urlparse(web_url).netloc
 
@@ -33,43 +31,56 @@ assets_files = []
 outside_links = []
 inside_links = []
 
+current_directory = Path(__file__).parent
+
+
 for script in soup.find_all("script"):
 	if script.attrs.get("src"):
-		
 		# if the tag has the attribute 
 		# 'src'
-		url = script.attrs.get("src")
-		js_files.append(url)
+		href = script.attrs.get("src")
+		js_files.append(href)
+		file_path = save_web_site_file(href, web_url, current_directory, 'js')
+		script['src'] = script['src'].replace(script['src'], file_path)
 
 for css in soup.find_all("link"):
 	if css.attrs.get("href"):
 		
 		# if the link tag has the 'href'
 		# attribute
-		_url = css.attrs.get("href")
-		if "css" in _url:
-			css_files.append(_url)
+		href = css.attrs.get("href")
+		if "css" in href:
+			css_files.append(href)
+			save_web_site_file(href, web_url, current_directory, 'css')
 		else:
-			assets_files.append(_url)
+			assets_files.append(href)
+			save_web_site_file(href, web_url, current_directory, 'images')
 
 
 for anchor in soup.find_all("a"):
 	href = anchor.attrs.get("href")
 	domain = urlparse(href).netloc
 	if web_url_domain != domain:
-		if href not in outside_links:
+		if href not in outside_links and "" != domain and domain != web_url_domain:
 			outside_links.append(href)
 		elif href not in inside_links and domain == "" or domain == web_url_domain:
 			inside_links.append(href)
 	elif href not in inside_links and "" != href:
 		inside_links.append(href)
 
-print("=" * 150)
-print(inside_links)
-print("=" * 150)
-print(outside_links)
-# print(str("*" * 50) + " js files "  + str("*" * 50))
-# print(js_files)
-# print(str("*" * 50) + " css files " + str("*" * 50))
-# print(css_files)
+for anchor in soup.find_all("img"):
+	src = anchor.attrs.get("src")
+	if src not in assets_files:
+		assets_files.append(src)
+		save_web_site_file(src, web_url, current_directory, 'images')
 
+# new_content = ""
+
+# for line in url.text.split('\n'):
+# 	print(line)
+# 	new_content += line
+
+# for a in soup.findAll('a'):
+# 	a['href'] = a['href'].replace(a['href'], "mysite")
+# 	break
+print(str(soup))
