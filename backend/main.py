2b09e4e021c5be4925dfa836160c1012bfeb7461
IMRAN BAALI <imran.baali19@gmail.com>
2021-07-28 15:01:57 +0100

    use wget to get website content

--- backend/main.py
+++ backend/main.py
@@ -1,86 +1,38 @@
-# Import Required Library
-from	os								import	link
-from	requests						import	get
-from	bs4								import	BeautifulSoup
-from	urllib.parse					import	urlparse
-from	create_public_assets_directory	import	create_public_assets_directory, save_web_site_file
-from	pathlib							import	Path
-from	os								import	path
+import os
+import zipfile
+from	urllib.parse	import	urlparse
+import shutil
 
-create_public_assets_directory()
-
-# Web URL
-# web_url = "https://www.kirwako.com/"
-web_url = "https://bookylia.com/"
-# web_url = "https://www.geeksforgeeks.org/how-to-extract-script-and-css-files-from-web-pages-in-python/"
-
-# get HTML content
-url = get(web_url)
-html = url.content
-# text = url.text
-
-web_url_domain = urlparse(web_url).netloc
-
-# parse HTML Content
-soup = BeautifulSoup(html, "html.parser")
-
-js_files = []
-css_files = []
 html_files = []
+css_files = []
+js_files = []
 assets_files = []
-outside_links = []
-inside_links = []
 
-current_directory = Path(__file__).parent
+def zipdir(path, ziph):
+	# ziph is zipfile handle
+	for root, dirs, files in os.walk(path):
+		for file in files:
+			if file.split('.')[-1] == "html":
+				html_files.append(os.path.join(root, file))
+			elif file.split('.')[-1] == "css":
+				css_files.append(os.path.join(root, file))
+			elif file.split('.')[-1] == "js":
+				js_files.append(os.path.join(root, file))
+			else:
+				assets_files.append(os.path.join(root, file))
+			ziph.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), os.path.join(path, '..')))
 
+website_url = "https://www.kirwako.com/"
 
-for script in soup.find_all("script"):
-	if script.attrs.get("src"):
-		# if the tag has the attribute 
-		# 'src'
-		href = script.attrs.get("src")
-		js_files.append(href)
-		file_path = save_web_site_file(href, web_url, current_directory, 'js')
-		script['src'] = script['src'].replace(script['src'], file_path)
+domain_name = urlparse(website_url).netloc
 
-for css in soup.find_all("link"):
-	if css.attrs.get("href"):
-		
-		# if the link tag has the 'href'
-		# attribute
-		href = css.attrs.get("href")
-		if "css" in href:
-			css_files.append(href)
-			save_web_site_file(href, web_url, current_directory, 'css')
-		else:
-			assets_files.append(href)
-			save_web_site_file(href, web_url, current_directory, 'images')
+# download all pages
+# os.system("wget -mkEpnp --html-extension --restrict-file-names=windows " + website_url)
 
+# download just the sepefied page
+os.system("wget -rpk --html-extension --restrict-file-names=windows " + website_url)
 
-for anchor in soup.find_all("a"):
-	href = anchor.attrs.get("href")
-	domain = urlparse(href).netloc
-	if web_url_domain != domain:
-		if href not in outside_links and "" != domain and domain != web_url_domain:
-			outside_links.append(href)
-		elif href not in inside_links and domain == "" or domain == web_url_domain:
-			inside_links.append(href)
-	elif href not in inside_links and "" != href:
-		inside_links.append(href)
-
-for anchor in soup.find_all("img"):
-	src = anchor.attrs.get("src")
-	if src not in assets_files:
-		assets_files.append(src)
-		save_web_site_file(src, web_url, current_directory, 'images')
-
-# new_content = ""
-
-# for line in url.text.split('\n'):
-# 	print(line)
-# 	new_content += line
-
-# for a in soup.findAll('a'):
-# 	a['href'] = a['href'].replace(a['href'], "mysite")
-# 	break
-print(str(soup))
+zipf = zipfile.ZipFile("/tmp/" + domain_name + ".zip", "w", zipfile.ZIP_DEFLATED)
+zipdir(domain_name + '/', zipf)
+zipf.close()
+shutil.rmtree(domain_name)
